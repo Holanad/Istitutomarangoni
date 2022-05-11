23887ebe94b617a58bd152128a4f05f1a2f37b12
Vladislav <volkov99.at@gmail.com>
2022-05-12 02:05:34 +0300

    ewewew

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,11 +22,14 @@ const swiper = new Swiper(".recall-slider", {
 const sliderRecall = () => {
     let countSlide = document.querySelectorAll('.recall-item').length;
     let recallItemCount = document.querySelector('.recall-item-count');
-    
-    if (countSlide <= 9) {
-        recallItemCount.innerHTML = '0' + countSlide;
+    if(recallItemCount === null) {
+        return 0;
     } else {
-        recallItemCount.innerHTML = countSlide;
+        if (countSlide <= 9) {
+            recallItemCount.innerHTML = '0' + countSlide;
+        } else {
+            recallItemCount.innerHTML = countSlide;
+        }
     }
 };
 sliderRecall();
@@ -74,6 +77,75 @@ scrollTeacher();*/
 
 // jQuery function
 $(document).ready(function() {
+    // dataSelects
+    function dataSelects () {
+        function newsSelect () {      
+            $('.news-filter__btn').click(function (e) {
+                let inputNews = $('.news-filter__input');
+                let button = $('.news-filter__btn span');
+                e.preventDefault();
+                $('.news-filter-select').toggleClass('active');
+                $('.news-filter-select__item').click(function () {
+                    let dataItem = $(this).text().trim();
+                    console.log($(this).text().trim())
+                    inputNews.val(dataItem);
+                    button.text(dataItem);
+                    $('.news-filter-select').removeClass('active');
+                })
+            });
+        }
+        newsSelect();
+        //
+        function recallSelectCity () {      
+            $('.recall-filter__btn-city').click(function (e) {
+                let inputNews = $('.recall-filter__input-city');
+                let button = $('.recall-filter__btn-city span');
+                e.preventDefault();
+                $('.recall-filter-select-city').toggleClass('active');
+                $('.recall-filter-select__item-city').click(function () {
+                    let dataItem = $(this).text().trim();
+                    console.log($(this).text().trim())
+                    inputNews.val(dataItem);
+                    button.text(dataItem);
+                    $('.recall-filter-select-city').removeClass('active');
+                })
+            });
+        }
+        recallSelectCity();
+        function recallSelectEducation () {      
+            $('.recall-filter__btn-education').click(function (e) {
+                let inputNews = $('.recall-filter__input-education');
+                let button = $('.recall-filter__btn-education span');
+                e.preventDefault();
+                $('.recall-filter-select-education').toggleClass('active');
+                $('.recall-filter-select__item-education').click(function () {
+                    let dataItem = $(this).text().trim();
+                    console.log($(this).text().trim())
+                    inputNews.val(dataItem);
+                    button.text(dataItem);
+                    $('.recall-filter-select-education').removeClass('active');
+                })
+            });
+        }
+        recallSelectEducation();
+        function recallSelectDirection () {      
+            $('.recall-filter__btn-direction').click(function (e) {
+                let inputNews = $('.recall-filter__input-direction');
+                let button = $('.recall-filter__btn-direction span');
+                e.preventDefault();
+                $('.recall-filter-select-direction').toggleClass('active');
+                $('.recall-filter-select__item-direction').click(function () {
+                    let dataItem = $(this).text().trim();
+                    console.log($(this).text().trim())
+                    inputNews.val(dataItem);
+                    button.text(dataItem);
+                    $('.recall-filter-select-direction').removeClass('active');
+                })
+            });
+        }
+        recallSelectDirection();
+    }
+    dataSelects();
     // accordion
     function accordion() {
         $(".price-item-head").click(function(){
@@ -84,15 +156,15 @@ $(document).ready(function() {
     accordion();
     // activeSelect
     function activeSelect () {
-        $(".intro-form-elem__select").each(function() {
+        $(".select-form-elem__select").each(function() {
             $(this).click(function () {
                 if ($(this).parent().hasClass('active')) {                   
                     $(this).parent().removeClass('active');
                 } else {
-                    $('.intro-form-elem__select').parent().removeClass('active');
+                    $('.select-form-elem__select').parent().removeClass('active');
                     $(this).parent().toggleClass('active');
                 }
-            })
+            });
         });
     }
     activeSelect();
